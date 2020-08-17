6c95c2fd387a02c18effdf7aa29458ac568a52ac
biginsect <love2008dodo@gmail.com>
2020-08-17 23:46:02 +0800

    use SharedPreference for remember password.

--- SimpleBroadcastDemo/app/src/main/java/com/biginsect/simplebroadcastdemo/LoginActivity.kt
+++ SimpleBroadcastDemo/app/src/main/java/com/biginsect/simplebroadcastdemo/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.biginsect.simplebroadcastdemo
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
@@ -12,18 +14,43 @@ import kotlinx.android.synthetic.main.activity_login.*
  */
 class LoginActivity : BaseActivity() {
 
+    private lateinit var mPrefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mPrefs = getPreferences(Context.MODE_PRIVATE)
+        val isRemember = mPrefs.getBoolean("remember_password", false)
+        if (isRemember) {
+            val account = mPrefs.getString("account", "")
+            val password = mPrefs.getString("password", "")
+            accountEdit.setText(account)
+            passwordEdit.setText(password)
+            rememberPass.isChecked = true
+        }
         loginBtn.setOnClickListener {
-            val account = accountEdit.text.toString()
-            val password = passwordEdit.text.toString()
-            if ("Admin" == account && "123" == password) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+            login()
+        }
+    }
+
+    private fun login() {
+        val account = accountEdit.text.toString()
+        val password = passwordEdit.text.toString()
+        if ("Admin" == account && "123" == password) {
+            val editor = mPrefs.edit()
+            if (rememberPass.isChecked) {
+                editor.putBoolean("remember_password", true)
+                editor.putString("account", account)
+                editor.putString("password", password)
             } else {
-                Toast.makeText(this, "Account or password is invalid.", Toast.LENGTH_SHORT).show()
+                editor.clear()
             }
+            editor.apply()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Account or password is invalid.", Toast.LENGTH_SHORT).show()
         }
     }
 
