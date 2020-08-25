4713a0e7c03d341b97420fcf3fdb3cb18f66ba70
biginsect <love2008dodo@gmail.com>
2020-08-25 22:58:29 +0800

    permission runtime.

--- SimpleBroadcastDemo/app/src/main/java/com/biginsect/simplebroadcastdemo/MainActivity.kt
+++ SimpleBroadcastDemo/app/src/main/java/com/biginsect/simplebroadcastdemo/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.biginsect.simplebroadcastdemo
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -14,8 +20,37 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         forceOffline.setOnClickListener {
-            val intent = Intent("com.biginsect.simplebroadcastdemo.FORCE_OFFLINE")
-            sendBroadcast(intent)
+//            val intent = Intent("com.biginsect.simplebroadcastdemo.FORCE_OFFLINE")
+//            sendBroadcast(intent)
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+            }else {
+                call()
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    call()
+                }else{
+                    Toast.makeText(this, "Deny permission: call phone", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
